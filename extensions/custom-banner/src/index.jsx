a4901860ef29eb8e77d602edcfa3133d00e22fe1
Elizabeth Kenyon <elizabeth.kenyon@shopify.com>
2023-07-20 17:15:32 -0500

    first commit

---
+++ extensions/custom-banner/src/index.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import {
+  reactExtension,
+  Banner,
+  useSettings,
+} from "@shopify/ui-extensions-react/checkout";
+
+// [START custom-banner.ext-point]
+// Set the entry points for the extension
+const checkoutBlock = reactExtension("purchase.checkout.block.render", () => <App />);
+export { checkoutBlock };
+
+const deliveryAddress = reactExtension("purchase.checkout.delivery-address.render-before", () => <App />);
+export { deliveryAddress };
+// [END custom-banner.ext-point]
+
+function App() {
+  // [START custom-banner.use-settings]
+  // Use the merchant-defined settings to retrieve the extension's content
+  const {title, description, collapsible, status: merchantStatus} = useSettings();
+
+  // Set a default status for the banner if a merchant didn't configure the banner in the checkout editor
+  const status = merchantStatus ?? 'info';
+  // [END custom-banner.use-settings]
+
+  // [START custom-banner.render]
+  // Render the banner
+  return (
+    <Banner title={title} status={status} collapsible={collapsible}>
+      {description}
+    </Banner>
+  );
+// [END custom-banner.render]
+}
